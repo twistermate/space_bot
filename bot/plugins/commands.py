d4ba31c6e5d0b9c1352c046e5847f0346b461f8f
moviesworldi <70133936+moviesworldi@users.noreply.github.com>
2021-07-11 18:54:00 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @MW_Linkz,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔰 MOVIESWORLD Team 🔰', url="https://t.me/MW_Linkz"
                                 )
                         ]
                     ]
