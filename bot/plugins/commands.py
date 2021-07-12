2f709f1a0fc366b9f315bdfc858faf7cfbba427b
moviesworldi <70133936+moviesworldi@users.noreply.github.com>
2021-07-12 16:23:24 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = 
+                caption = caption,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
