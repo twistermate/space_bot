5081e57fc23b17365c41e26e85763c92aed29623
moviesworldi <70133936+moviesworldi@users.noreply.github.com>
2021-07-13 10:43:59 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
+        InlineKeyboardButton('Developers', url='https://t.me/MW_Linkz'),
         InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/MOVIESWORLDI')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
