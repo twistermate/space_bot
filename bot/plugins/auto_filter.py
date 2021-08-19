0858f40324631ae612106af9076f2dc10c086289
Devil Hacker <82395901+rakeshyt@users.noreply.github.com>
2021-08-19 14:38:08 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -81,7 +81,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"📁{file_size} 📒{file_name}"
+            button_text = f"📁{file_size}📒{file_name}"
             
 
             if file_type == "video":
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"✨ Check links at @MW_Linkz ✨ Found {(len_results)} Results For Your Request 😊: <code>{query}</code>",
+                text=f"✨ Check links at @TheModdingGod ✨ Found {(len_results)} Results For Your Request 😊: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
