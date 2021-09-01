77766ebf23ba7da8ab0981d6198909568bda535b
twistermate <88696997+twistermate@users.noreply.github.com>
2021-09-01 15:56:40 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -147,7 +147,7 @@ async def auto_filter(bot, update):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("🎖️ Go to Next Page 🎖️", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("Go to Next Page", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"✨ Check links at @TheModdingGod ✨ Found {(len_results)} Results For Your Request 😊: <code>{query}</code>",
+                text=f"✨ Join @closechapters2_0 ✨ Found {(len_results)} Results For Your Request 😊: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
