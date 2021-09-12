9330b927ff72c0cc17c7c54add96c9e3cb4f8acd
twistermate <88696997+twistermate@users.noreply.github.com>
2021-09-12 22:46:53 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -14,40 +14,20 @@ For More Details Click Help Button Below..
 </i>"""    
     
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
+<b><i><u>GET LOST IAM Different bot u know😤!?</u></i></b>
 
-<b>Bot Commands (Works Only In Groups) :</b>
 
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
 
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> 😈𓆩𝕯𝖊𝖛𝖎𝖑 𝕳𝖆𝖈𝖐𝖊𝖗𓆪😈 Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> HHT Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/DevilHackerRakesh">😈𓆩𝕯𝖊𝖛𝖎𝖑 𝕳𝖆𝖈𝖐𝖊𝖗𓆪😈</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Makerwith">Irfan</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/rakeshyt/DevilHacker">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://t.me/joinchat/eZk8OSmHwR5jYTJl">Click Me</a></i>
 """
